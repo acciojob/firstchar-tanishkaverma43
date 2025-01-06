14006406df0460adc75a26cad2b15e1bd5416586
Tanishka Verma <152250719+tanishkaverma43@users.noreply.github.com>
2025-01-06 11:14:32 +0530

    created a submission

--- script.js
+++ script.js
@@ -3,7 +3,11 @@ function firstChar(text) {
 	let texty = text.trim();
 	return texty.charAt(0);
 }
-console.log(firstChar(" Hello World")); 
+let text = " Hello World";
+
+console.log(firstChar(text)); // This should work
+
+alert(firstChar(text));
 
 // Do not change the code below
 //Uncomment the following line to show the prompt popup
